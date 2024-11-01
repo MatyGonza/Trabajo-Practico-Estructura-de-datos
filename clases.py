4c532457f1cab2217289ad460b17cbe9ed209ac2
michaeljunior05 <jf6610977@gmail.com>
2024-11-01 16:44:56 -0300

    Update clases.py

--- clases.py
+++ clases.py
@@ -2,5 +2,7 @@ class Personaje:
   def __init__(self,nombre:str,raza:str,nivel_de_poder:int,habilidades):
     self.nombre = nombre
     self.raza = raza
-    self.nivel_de_poder
+    self.nivel_de_poder = nivle_de_poder}
+    self.habilidades = []
+    
     
