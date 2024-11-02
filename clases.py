770b0269f437412aaafe9a1e48fc2bd764c1f2fb
matigonza <gonzalezmatias879@gmail.com>
2024-11-02 12:16:55 -0300

    Se agrego metodos a la clase Personaje que estaban en el diagrama y tambien se le agrego el atribito vida que es un entero, tambien se agregaron la clase humano y la clase andriode, Se modifico el Diagrama agregandole el atributo vida a la clase Personaje

--- clases.py
+++ clases.py
@@ -1,8 +1,38 @@
+
+
+""" 
+Agregue a la clase personaje el atributo vida que es un entero
+agregue los nombres de los metodos que estaban en el diagrama 
+agregue dos clases una humano y la otra androide como prueba estas heredan de Personaje sus atributos y metodos
+"""
+
 class Personaje:
-  def __init__(self,nombre:str,raza:str,nivel_de_poder:int,habilidades):
+  def __init__(self,nombre:str,vida:int,raza:str,nivel_de_poder:int,habilidades):
     self.nombre = nombre
+    self.vida = vida
     self.raza = raza
-    self.nivel_de_poder = nivle_de_poder}
+    self.nivel_de_poder = nivel_de_poder
     self.habilidades = []
     
+    def atacar(self):
+      pass
     
+    def defender(self):
+      pass
+    
+    def usar_tecnica(self):
+      pass
+    
+    def recibir_daño(self):
+      pass
+    
+    def mostrar_estado(self):
+      pass
+    
+class Humano(Personaje):
+  def __init__(self, nombre: str, vida: int, raza: str, nivel_de_poder: int, habilidades):
+    super().__init__(nombre, vida, "Humano", nivel_de_poder, habilidades)
+    
+class Androide(Personaje):
+  def __init__(self, nombre: str, vida: int, raza: str, nivel_de_poder: int, habilidades):
+    super().__init__(nombre, vida, "Andriode", nivel_de_poder, habilidades)
