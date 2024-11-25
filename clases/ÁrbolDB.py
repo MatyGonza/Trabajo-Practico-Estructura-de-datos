c0c7f8c1ac0acc6f1b00a1f9c2bbea729f99ffe5
michaeljunior <jf6610977@gmail.com>
2024-11-25 16:41:41 -0300

    se mejoraron

--- clases/ÁrbolDB.py
+++ clases/ÁrbolDB.py
@@ -1,14 +1,23 @@
 # Se define la clase "Personaje" que contendrá como atributos el nombre y nivel de poder de los personajes. 
-class Personaje:
-    def __init__(self, nombre, nivel_poder):
-        self.nombre = nombre
-        self.nivel_poder = nivel_poder
+#class Personaje:
+    #def __init__(self, nombre, nivel_poder):
+        #self.nombre = nombre
+        #self.nivel_poder = nivel_poder
+
+
+
+
 
 # Se define la estructura de cada nodo del árbol binario. Cada nodo contendrá la información del personaje (nombre y nivel de poder) y las referencias a sus hijos (izquierdo y derecho).
 class Nodo:
     def __init__(self, personaje):
+        """
+        El nodo contiene los atributos un personaje y dos hijos,
+        uno a la izquierda y otro a la izquierda.
+
+        """
         self.personaje = personaje
-        self.izquierda = None
+        self.izquierda = None 
         self.derecha = None
 
 # Se crea la clase del árbol binario, la cual se encargará de gestionar la inserción de nuevos nodos y la búsqueda de personajes.
@@ -25,15 +34,21 @@ class ArbolBinario:
             self._insertar_recursivo(self.raiz, personaje)
     
     def _insertar_recursivo(self, nodo, personaje):
+        #Si el nivel de poder del personaje es menor que el del nodo actual
         if personaje.nivel_poder < nodo.personaje.nivel_poder:
             if nodo.izquierda is None:
+                # Si el hijo izquierdo está vacío, colocamos al personaje allí
                 nodo.izquierda = Nodo(personaje)
             else:
+                # Si el hijo izquierdo ya tiene un nodo, seguimos buscando recursivamente en la izquierda
                 self._insertar_recursivo(nodo.izquierda, personaje)
         else:
+            # Si el nivel de poder del personaje es mayor o igual, lo insertamos en el subárbol derecho
             if nodo.derecha is None:
+                # Si el hijo derecho está vacío, colocamos al personaje allí
                 nodo.derecha = Nodo(personaje)
             else:
+                 # Si el hijo derecho ya tiene un nodo, seguimos buscando recursivamente en la derecha
                 self._insertar_recursivo(nodo.derecha, personaje)
 
 # Método de búsqueda que permite encontrar personajes según su nivel de poder, utilizando un método recursivo.
@@ -41,6 +56,7 @@ class ArbolBinario:
         return self._buscar_personaje_recursivo(self.raiz, nivel_poder)
     
     def _buscar_personaje_recursivo(self, nodo, nivel_poder):
+        # Si hemos llegado a un nodo vacío, significa que el personaje no está en el árbol
         if nodo is None:
             return None
         if nodo.personaje.nivel_poder == nivel_poder:
